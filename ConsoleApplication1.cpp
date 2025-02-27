bf596f76cf9e8d9d8fbe888ede24f04a86677353
Aziz Dev <animeshnick15@gmail.com>
2025-02-27 19:38:41 +0400

    ADD FILE

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
-using namespace std;
 
 int main() 
 {
-	cout << "Hello World";
+	setlocale(LC_ALL, "Rus");
+
+	int x = 5;
+	int y = 5;
+	
+	std::cout << "Сумма чисел: " << x + y;
+	std::cout << "\nПроизведение чисел: " << x * y;
+	std::cout << "\nДеление чисел: " << x / y;
+	
 }
